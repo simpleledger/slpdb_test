27dfd0acbc27abcc22cd3d19ca94ea207eecacc7
blockparty <hello@blockparty.sh>
2019-04-16 15:20:43 -0500

    additional tests and refactor

--- test.js
+++ test.js
@@ -17,38 +17,6 @@ const slpdb = {
 }
 
 describe('tokens', () => {
-  describe('#tokenDetails exists', () => {
-    it('there should be no token documents without a tokenDetails property', () =>
-      slpdb.query({
-        "v": 3,
-        "q": {
-          "db": ["t"],
-          "find": {
-            "tokenDetails": {
-              "$exists": false
-            }
-          },
-          "limit": 10
-        }
-      }).then((data) => assert.equal(0, data.t.length))
-    )
-  });
-  describe('#tokenStats exists', () => {
-    it('there should be no token documents without a tokenStats property', () =>
-      slpdb.query({
-        "v": 3,
-        "q": {
-          "db": ["t"],
-          "find": {
-            "tokenStats": {
-              "$exists": false
-            }
-          },
-          "limit": 10
-        }
-      }).then((data) => assert.equal(0, data.t.length))
-    )
-  });
   describe('#mintBatonUtxo correct format', () => {
     it('mintBatonUtxo must be either empty string or follow the regex provided for txid:vout', () =>
       Promise.all([
@@ -95,68 +63,52 @@ describe('tokens', () => {
       ]).then(([total, mint_baton_matched]) => assert.equal(total.t.mintBatonUtxo, mint_baton_matched.t.mintBatonUtxo))
     )
   });
-  describe('#tokenDetails.decimals correct format', () => {
-    it('decimals must be 0-9', () =>
-      slpdb.query({
-        "v": 3,
-        "q": {
-          "db": ["t"],
-          "aggregate": [
-            {
-              "$match": {
-                "$or": [
-                  {
-                    "tokenDetails.decimals": {
-                      "$gt": 9
-                    }
-                  },
-                  {
-                    "tokenDetails.decimals": {
-                      "$lt": 0
-                    }
-                  },
-                  {
-                    "tokenDetails.decimals": {
-                      "$not": {
-                        "$type": "number"
-                      }
-                    }
-                  }
-                ]
-              }
-            },
-            {
-              "$count": "mintBatonUtxo"
-            }
-          ],
-          "limit": 1
-        }
-      }).then(data => assert.equal(0, data.t.length))
-	)
-  })
-  describe('#tokenDetails.decimals correct format', () => {
-    it('decimals must be 0-9', () =>
-      Promise.all([
-		slpdb.query({
-          "v": 3,
-          "q": {
-            "aggregate": [
-              {
-                "$count": "mintBatonUtxo"
-              }
-            ],
-            "limit": 1
-          }
-        }),
+
+  describe('tokens.tokenDetails', () => {
+    describe('#tokenDetails exists', () => {
+      it('there should be no token documents without a tokenDetails property', () =>
         slpdb.query({
           "v": 3,
           "q": {
+            "db": ["t"],
+            "find": {
+              "tokenDetails": {
+                "$exists": false
+              }
+            },
+            "limit": 10
+          }
+        }).then((data) => assert.equal(0, data.t.length))
+      )
+    });
+    describe('#tokenDetails.decimals correct format', () => {
+      it('decimals must be 0-9', () =>
+        slpdb.query({
+          "v": 3,
+          "q": {
+            "db": ["t"],
             "aggregate": [
               {
                 "$match": {
-                  "tokenDetails.tokenIdHex": {
-                  "$regex": "^[0-9a-f]{64}$"
-                  }
+                  "$or": [
+                    {
+                      "tokenDetails.decimals": {
+                        "$gt": 9
+                      }
+                    },
+                    {
+                      "tokenDetails.decimals": {
+                        "$lt": 0
+                      }
+                    },
+                    {
+                      "tokenDetails.decimals": {
+                        "$not": {
+                          "$type": "number"
+                        }
+                      }
+                    }
+                  ]
                 }
               },
               {
@@ -165,9 +117,79 @@ describe('tokens', () => {
             ],
             "limit": 1
           }
-        })
-      ]).then(([total, tokenidhex_matched]) => assert.equal(total.t.mintBatonUtxo, tokenidhex_matched.t.mintBatonUtxo))
-	)
+        }).then(data => assert.equal(0, data.t.length))
+      )
+    })
+    describe('#tokenDetails.tokenIdHex correct format', () => {
+      it('tokenIdHex must exist and must be hex string of 64 length', () =>
+        Promise.all([
+          slpdb.query({
+            "v": 3,
+            "q": {
+              "aggregate": [
+                {
+                  "$count": "mintBatonUtxo"
+                }
+              ],
+              "limit": 1
+            }
+          }),
+          slpdb.query({
+            "v": 3,
+            "q": {
+              "aggregate": [
+                {
+                  "$match": {
+                    "tokenDetails.tokenIdHex": {
+                    "$regex": "^[0-9a-f]{64}$"
+                    }
+                  }
+                },
+                {
+                  "$count": "mintBatonUtxo"
+                }
+              ],
+              "limit": 1
+            }
+          })
+        ]).then(([total, tokenidhex_matched]) => assert.equal(total.t.mintBatonUtxo, tokenidhex_matched.t.mintBatonUtxo))
+      )
+    })
+    describe('#tokenDetails.transactionType correct format', () => {
+      it('transactionType must be GENESIS', () =>
+        slpdb.query({
+          "v": 3,
+          "q": {
+            "db": ["t"],
+            "find": {
+              "tokenDetails.transactionType": {
+                "$nin": ["GENESIS"]
+              }
+            },
+            "limit": 1
+          }
+        }).then((data) => assert.equal(0, data.t.length))
+        )
+    })
   })
+
+  describe('tokens.tokenStats', () => {
+    describe('#tokenStats exists', () => {
+      it('there should be no token documents without a tokenStats property', () =>
+        slpdb.query({
+          "v": 3,
+          "q": {
+            "db": ["t"],
+            "find": {
+              "tokenStats": {
+                "$exists": false
+              }
+            },
+            "limit": 10
+          }
+        }).then((data) => assert.equal(0, data.t.length))
+      )
+    });
+  });
 });
 
